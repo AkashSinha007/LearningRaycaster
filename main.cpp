2b5572ef174792e2ea498b9fb5eeffb184ab8664
AkashSinha007 <akashkumarsinha007@gmail.com>
2024-04-09 10:55:40 +0200

    Finished coding by Adding Ray casts and organized code

--- main.cpp
+++ main.cpp
@@ -7,9 +7,11 @@
 #define PI 3.1415926535
 
 
-int  mapX = 8, mapY = 8, mapS = 64;
-
-int map[]=           //the map array.
+//-----------------------------MAP----------------------------------------------
+#define mapX  8      //map width
+#define mapY  8      //map height
+#define mapS 64      //map cube size
+int map[]=           //the map array. Edit to change level but keep the outer walls
 {
  1,1,1,1,1,1,1,1,
  1,0,1,0,0,0,0,1,
@@ -20,79 +22,6 @@ int map[]=           //the map array.
  1,0,0,0,0,0,0,1,
  1,1,1,1,1,1,1,1,	
 };
-
-
-float px, py, pdx,pdy,pa; //player position
-
-void init()
-{
-    glClearColor(0.3,0.3,0.3,0);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0, 1024, 512,0, -1, 1);
-    glMatrixMode(GL_MODELVIEW);
-
-    px = 300; 
-    py = 300;
-    pdx=cos(pa)*5;
-    pdy=sin(pa)*5;
-}
-
-void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_A && action == GLFW_PRESS)
-    {
-        pa-= 0.1;
-        if(pa <0)
-        {
-            pa+= 2*PI;
-        }
-        pdx = cos(pa)*5;
-        pdy = sin(pa)*5;
-    }
-
-    if (key == GLFW_KEY_D && action == GLFW_PRESS)
-    {
-        pa+= 0.1;
-        if(pa > 2*PI)
-        {
-            pa-= 2*PI;
-        }
-        pdx = cos(pa)*5;
-        pdy = sin(pa)*5;
-    }
-
-    if (key == GLFW_KEY_W && action == GLFW_PRESS)
-    {
-        px += pdx;
-        py += pdy;
-    }
-
-    if (key == GLFW_KEY_S && action == GLFW_PRESS)
-    {
-        px -= pdx;
-        py -= pdy;
-    }
-    
-    //glfwSetWindowShouldClose(window, GLFW_TRUE);
-    // Add more key handling here if needed
-}
-
-void drawPlayer()
-{
-    glColor3f(1,1,0);
-    glPointSize(10);
-    glBegin(GL_POINTS);
-    glVertex2i(px,py);
-    glEnd();
-
-    glLineWidth(3);
-    glBegin(GL_LINES);
-    glVertex2i(px,py);
-    glVertex2i(px+pdx*5, py+pdy*5);
-    glEnd();
-}
-
 
 void drawMap2D()
 {
@@ -111,6 +40,204 @@ void drawMap2D()
    glEnd();
   } 
  } 
+}//-----------------------------------------------------------------------------
+
+//------------------------PLAYER------------------------------------------------
+
+float degToRad(int a) 
+{ 
+    return a*M_PI/180.0;
+}
+int FixAng(int a)
+{ 
+    if(a>359)
+    { 
+        a-=360;
+    } 
+    if(a<0)
+    { 
+        a+=360;
+    } 
+    return a;
+}
+
+float px, py, pdx,pdy,pa; //player position and angles 
+bool isAPressed = false; //Represents Key 'A' and 'Left' direction
+bool isDPressed = false; //Represents Key 'D' and 'Right' direction
+bool isWPressed = false; //Represents Key 'W' and 'Up' direction
+bool isSPressed = false; //Represents Key 'S' and 'Down' direction
+
+void drawPlayer2D()
+{
+    glColor3f(1,1,0);
+    glPointSize(10);
+    glLineWidth(4);
+    glBegin(GL_POINTS);
+    glVertex2i(px,py);
+    glEnd();
+
+    glBegin(GL_LINES);
+    glVertex2i(px,py);
+    glVertex2i(px+pdx*20, py+pdy*20);
+    glEnd();
+}
+
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_A)
+    {
+        if (action == GLFW_PRESS)
+        {
+            isAPressed = true;
+        }
+        else if (action == GLFW_RELEASE)
+        {
+            isAPressed = false;
+        }
+    }
+
+    if (key == GLFW_KEY_D)
+    {
+        if (action == GLFW_PRESS)
+        {
+            isDPressed = true;
+        }
+        else if (action == GLFW_RELEASE)
+        {
+            isDPressed = false;
+        }
+    }
+
+    if (key == GLFW_KEY_W)
+    {
+        if (action == GLFW_PRESS)
+        {
+            isWPressed = true;
+        }
+        else if (action == GLFW_RELEASE)
+        {
+            isWPressed = false;
+        }
+    }
+
+    if (key == GLFW_KEY_S)
+    {
+        if (action == GLFW_PRESS)
+        {
+            isSPressed = true;
+        }
+        else if (action == GLFW_RELEASE)
+        {
+            isSPressed = false;
+        }
+    }
+}
+
+void updatePlayerPosition()
+{
+    if (isAPressed)
+    {
+        pa += 5;
+        pa = FixAng(pa);
+        pdx = cos(degToRad(pa));
+        pdy = -sin(degToRad(pa));
+    }
+    if (isDPressed)
+    {
+        pa-=5; 
+        pa=FixAng(pa); 
+        pdx=cos(degToRad(pa)); 
+        pdy=-sin(degToRad(pa));
+    }
+
+    if (isWPressed)
+    {
+        px+=pdx*5; 
+        py+=pdy*5;
+    }
+
+    if (isSPressed)
+    {
+        px-=pdx*5; 
+        py-=pdy*5;
+    }
+}
+
+
+//-----------------------------------------------------------------------------
+
+
+//---------------------------Draw Rays and Walls--------------------------------
+
+void drawRays2D()
+{
+ glColor3f(0,1,1); glBegin(GL_QUADS); glVertex2i(526,  0); glVertex2i(1006,  0); glVertex2i(1006,160); glVertex2i(526,160); glEnd();	
+ glColor3f(0,0,1); glBegin(GL_QUADS); glVertex2i(526,160); glVertex2i(1006,160); glVertex2i(1006,320); glVertex2i(526,320); glEnd();	 	
+	
+ int r,mx,my,mp,dof,side; float vx,vy,rx,ry,ra,xo,yo,disV,disH; 
+ 
+ ra=FixAng(pa+30);                                                              //ray set back 30 degrees
+ 
+ for(r=0;r<60;r++)
+ {
+  //---Vertical--- 
+  dof=0; side=0; disV=100000;
+  float Tan=tan(degToRad(ra));
+       if(cos(degToRad(ra))> 0.001){ rx=(((int)px>>6)<<6)+64;      ry=(px-rx)*Tan+py; xo= 64; yo=-xo*Tan;}//looking left
+  else if(cos(degToRad(ra))<-0.001){ rx=(((int)px>>6)<<6) -0.0001; ry=(px-rx)*Tan+py; xo=-64; yo=-xo*Tan;}//looking right
+  else { rx=px; ry=py; dof=8;}                                                  //looking up or down. no hit  
+
+  while(dof<8) 
+  { 
+   mx=(int)(rx)>>6; my=(int)(ry)>>6; mp=my*mapX+mx;                     
+   if(mp>0 && mp<mapX*mapY && map[mp]==1){ dof=8; disV=cos(degToRad(ra))*(rx-px)-sin(degToRad(ra))*(ry-py);}//hit         
+   else{ rx+=xo; ry+=yo; dof+=1;}                                               //check next horizontal
+  } 
+  vx=rx; vy=ry;
+
+  //---Horizontal---
+  dof=0; disH=100000;
+  Tan=1.0/Tan; 
+       if(sin(degToRad(ra))> 0.001){ ry=(((int)py>>6)<<6) -0.0001; rx=(py-ry)*Tan+px; yo=-64; xo=-yo*Tan;}//looking up 
+  else if(sin(degToRad(ra))<-0.001){ ry=(((int)py>>6)<<6)+64;      rx=(py-ry)*Tan+px; yo= 64; xo=-yo*Tan;}//looking down
+  else{ rx=px; ry=py; dof=8;}                                                   //looking straight left or right
+ 
+  while(dof<8) 
+  { 
+   mx=(int)(rx)>>6; my=(int)(ry)>>6; mp=my*mapX+mx;                          
+   if(mp>0 && mp<mapX*mapY && map[mp]==1){ dof=8; disH=cos(degToRad(ra))*(rx-px)-sin(degToRad(ra))*(ry-py);}//hit         
+   else{ rx+=xo; ry+=yo; dof+=1;}                                               //check next horizontal
+  } 
+  
+  glColor3f(0,0.8,0);
+  if(disV<disH){ rx=vx; ry=vy; disH=disV; glColor3f(0,0.6,0);}                  //horizontal hit first
+  glLineWidth(2); glBegin(GL_LINES); glVertex2i(px,py); glVertex2i(rx,ry); glEnd();//draw 2D ray
+    
+  int ca=FixAng(pa-ra); disH=disH*cos(degToRad(ca));                            //fix fisheye 
+  int lineH = (mapS*320)/(disH); if(lineH>320){ lineH=320;}                     //line height and limit
+  int lineOff = 160 - (lineH>>1);                                               //line offset
+  
+  glLineWidth(8);glBegin(GL_LINES);glVertex2i(r*8+530,lineOff);glVertex2i(r*8+530,lineOff+lineH);glEnd();//draw vertical wall  
+
+  ra=FixAng(ra-1);                                                              //go to next ray
+ }
+}//-----------------------------------------------------------------------------
+
+
+void init()
+{
+    glClearColor(0.3,0.3,0.3,0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, 1024, 512,0, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+
+    px = 150; 
+    py = 400;
+    pa = 90;
+    pdx=cos(degToRad(pa)); 
+    pdy=-sin(degToRad(pa)); 
 }
 
 void display(GLFWwindow* window)
@@ -121,8 +248,13 @@ void display(GLFWwindow* window)
     drawMap2D();
 
     /*draw Player*/
-    drawPlayer();
+    drawPlayer2D();
+
+    // Update player position based on press of WASD keys on keyboard
+    updatePlayerPosition();
     
+    /*draw Rays*/
+    drawRays2D();
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
